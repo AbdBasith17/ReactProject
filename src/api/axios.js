9b7fc531d763e7ca965d0598abe66803aa0a4316
Abdul Basith <abzit377@gmail.com>
2026-03-26 19:14:11 +0530

    backned acces chnage

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_API_URL || "https://13.51.114.178";
+const API_BASE = process.env.REACT_APP_API_URL || "https://perfaura-api.duckdns.org";
 
 const api = axios.create({
   baseURL: `${API_BASE}/api/`,
